d13c38966cb05ee449954df0ce3f75341b96664b
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-07-11 22:24:34 +0300

    enough for today..

--- src/js/common.js
+++ src/js/common.js
@@ -3,10 +3,9 @@ var md_sm_screen = window.matchMedia("(max-width: 767px)");
 
 
 
-$(window).resize(function () {
-  if (md_sm_screen.matches) {
-    $(document).ready(function () {
-      $('.bxslider').bxSlider();
-    });
-  }
-});
+
+if (md_sm_screen.matches) {
+  $(document).ready(function () {
+    $('.bxslider').bxSlider();
+  });
+}
